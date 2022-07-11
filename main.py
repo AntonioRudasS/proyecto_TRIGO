09d72554204a24d21790dbb684df2861eea96205
Antonio José Rudas Sanchez <62669845+AntonioRudasS@users.noreply.github.com>
2022-07-11 11:56:35 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+class Trigo:
+    cant_almacenada = 0
+    cant_plantada = 0
+    precio = 2
+    T_cosecha = 0
+    dinero = 0
+
+    def __init__(self, cant_almacenada, cant_plantada, precio, T_cosecha, dinero):
+        self.cant_almacenada = cant_almacenada
+        self.cant_plantada = cant_plantada
+        self.precio = precio
+        self.T_cosecha = T_cosecha
+        self.dinero = dinero
+    
+    def cosechar(self):
+        pass
+    
+    def plantar(self):
+        pass
+
+    def vender(self):
+        pass
+
+    def comprar(self):
+        pass
+
