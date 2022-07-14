c15e4826b32179a7cc83a277af2f514a68a5ae11
Antonio José Rudas Sanchez <62669845+AntonioRudasS@users.noreply.github.com>
2022-07-13 20:07:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-class Trigo:
-    cant_almacenada = 0
-    cant_plantada = 0
-    precio = 2
-    T_cosecha = 0
-    dinero = 0
+#importar la clase Trigo
+from clase_Trigo import Trigo
+#importar libreria para limpiar la consola
+import os
 
-    def __init__(self, cant_almacenada, cant_plantada, precio, T_cosecha, dinero):
-        self.cant_almacenada = cant_almacenada
-        self.cant_plantada = cant_plantada
-        self.precio = precio
-        self.T_cosecha = T_cosecha
-        self.dinero = dinero
+#def __init__(self, cant_almacenada, cant_plantada, precio, T_cosecha, dinero)
+objeto1 = Trigo(1, 0, 0, 0)
+fin = 0
+
+#funcion para limpiar la consola
+def ClearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+
+#bucle para ejecutar el ejercicio
+while (fin != 1):
+
+    ClearConsole()
+    objeto1.mostrar()
+    print("1.Plantar\n2.cosechar\n3.Vender\n4.Comprar")
+    opcion = input("elija: ")
+
+    if opcion == '1':
+        c_p = int(input("Cuantas unidades desea plantar?: "))
+        objeto1.plantar(c_p)
+        
+    elif opcion == '2':
+        objeto1.cosechar()
+        
+    elif opcion == '3':
+        c_v = int(input("Cuantas unidades desea vender?: "))
+        objeto1.vender(c_v)
+        
+    elif opcion == '4':
+        c_c = int(input("Cuantas unidades desea comprar?: "))
+        objeto1.comprar(c_c)
     
-    def cosechar(self):
-        pass
-    
-    def plantar(self):
-        pass
-
-    def vender(self):
-        pass
-
-    def comprar(self):
-        pass
+    else:
+        print("Esa opcion no es valida.")
+        
+    print("Desea salir?\n1. si\n2. no")
+    fin = int(input("Elija: "))
 
